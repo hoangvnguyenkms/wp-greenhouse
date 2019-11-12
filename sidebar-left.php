a46144551f73f37768154a8f6a5e9b0e8030bd00
hoangnguyenosd <hoang.nguyen.osd@gmail.com>
2019-11-12 10:52:33 +0700

    Fix color for left navbar

--- sidebar-left.php
+++ sidebar-left.php
@@ -3,16 +3,13 @@
 				<h4 class="title-box"><span>Sản phẩm</span></h4>
 				<ul class="sub-cat">
 					<li><a title="NHÀ KÍNH NÔNG NGHIỆP CÔNG NGHỆ CAO"
-							href="https://nhakinhtrongrau.vn/san-pham/nha-kinh-nong-nghiep-cong-nghe-cao">NHÀ KÍNH NÔNG
-							NGHIỆP CÔNG NGHỆ CAO</a></li>
+							href="https://nhakinhtrongrau.vn/san-pham/nha-kinh-nong-nghiep-cong-nghe-cao">Nhà Kính Nông Nghiệp Công Nghệ Cao</a></li>
 					<li><a title="THIẾT BỊ TƯỚI NÔNG NGHIỆP"
-							href="https://nhakinhtrongrau.vn/san-pham/thiet-bi-tuoi-nong-nghiep">THIẾT BỊ TƯỚI NÔNG
-							NGHIỆP</a></li>
+							href="https://nhakinhtrongrau.vn/san-pham/thiet-bi-tuoi-nong-nghiep">Thiết Bị Tưới Nông Nghiệp</a></li>
 					<li><a title="VẬT TƯ NHÀ KÍNH - NHÀ LƯỚI"
-							href="https://nhakinhtrongrau.vn/san-pham/vat-tu-nha-kinh-nha-luoi">VẬT TƯ NHÀ KÍNH - NHÀ
-							LƯỚI</a></li>
+							href="https://nhakinhtrongrau.vn/san-pham/vat-tu-nha-kinh-nha-luoi">Vật Tư Nhà Kính - Nhà Lưới</a></li>
 					<li class="last"><a title="CÔNG TRÌNH TIÊU BIỂU"
-							href="https://nhakinhtrongrau.vn/san-pham/cong-trinh-tieu-bieu">CÔNG TRÌNH TIÊU BIỂU</a>
+							href="https://nhakinhtrongrau.vn/san-pham/cong-trinh-tieu-bieu">Công Trình Tiêu Biểu</a>
 					</li>
 				</ul>
 				<div class="line-box">
