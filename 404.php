8b48cffffcfc99323a389af0f0432ff3878ecde3
hoangnguyenosd <hoang.nguyen.osd@gmail.com>
2019-11-12 14:51:00 +0700

    add side box and 404 page

---
+++ 404.php
@@ -0,0 +1,3 @@
+
+<?php $themeURL =  get_template_directory_uri(); ?>
+<img style="max-width: 100%, height: auto" src="<?php echo $themeURL; ?>/assets/images/404.png" alt="page not found" width="100%">
--- sidebar-left.php
+++ sidebar-left.php
@@ -1,44 +1,29 @@
 <?php $themeURL =  get_template_directory_uri(); ?>			
 			<div id="left-content">
-				<h4 class="title-box"><span>Sản phẩm</span></h4>
-				<ul class="sub-cat">
-					<li><a title="NHÀ KÍNH NÔNG NGHIỆP CÔNG NGHỆ CAO"
-							href="https://nhakinhtrongrau.vn/san-pham/nha-kinh-nong-nghiep-cong-nghe-cao">Nhà Kính Nông Nghiệp Công Nghệ Cao</a></li>
-					<li><a title="THIẾT BỊ TƯỚI NÔNG NGHIỆP"
-							href="https://nhakinhtrongrau.vn/san-pham/thiet-bi-tuoi-nong-nghiep">Thiết Bị Tưới Nông Nghiệp</a></li>
-					<li><a title="VẬT TƯ NHÀ KÍNH - NHÀ LƯỚI"
-							href="https://nhakinhtrongrau.vn/san-pham/vat-tu-nha-kinh-nha-luoi">Vật Tư Nhà Kính - Nhà Lưới</a></li>
-					<li class="last"><a title="CÔNG TRÌNH TIÊU BIỂU"
-							href="https://nhakinhtrongrau.vn/san-pham/cong-trinh-tieu-bieu">Công Trình Tiêu Biểu</a>
-					</li>
-				</ul>
-				<div class="line-box">
-					<div></div>
+				<div class="side-box">
+					<h4 class="title-box"><span>Sản phẩm</span></h4>
+					<ul class="sub-cat">
+						<li><a title="NHÀ KÍNH NÔNG NGHIỆP CÔNG NGHỆ CAO"
+								href="https://nhakinhtrongrau.vn/san-pham/nha-kinh-nong-nghiep-cong-nghe-cao">Nhà Kính Nông Nghiệp Công Nghệ Cao</a></li>
+						<li><a title="THIẾT BỊ TƯỚI NÔNG NGHIỆP"
+								href="https://nhakinhtrongrau.vn/san-pham/thiet-bi-tuoi-nong-nghiep">Thiết Bị Tưới Nông Nghiệp</a></li>
+						<li><a title="VẬT TƯ NHÀ KÍNH - NHÀ LƯỚI"
+								href="https://nhakinhtrongrau.vn/san-pham/vat-tu-nha-kinh-nha-luoi">Vật Tư Nhà Kính - Nhà Lưới</a></li>
+						<li class="last"><a title="CÔNG TRÌNH TIÊU BIỂU"
+								href="https://nhakinhtrongrau.vn/san-pham/cong-trinh-tieu-bieu">Công Trình Tiêu Biểu</a>
+						</li>
+					</ul>
 				</div>
 
-
-				<h4 class="title-box"><span>Thống kê truy cập</span></h4>
-				<ul class="statistics main-sub">
-					<li>Đang truy cập: 1</li>
-					<li>Trong ngày: 14</li>
-					<li>Hôm qua: 27</li>
-					<li>Tổng truy cập: 140677</li>
-					<li>Truy cập nhiều nhất: 431</li>
-					<li>Ngày nhiều nhất: 12.02.2016</li>
-				</ul>
-				<div class="line-box">
-					<div></div>
-				</div>
-				<h4 class="title-box"><span>Phụ kiện</span></h4>
-				<ul class="bg-adv main-sub">
-					<li><a href="https://nhakinhtrongrau.vn/trang-chu" title="BĂNG KEO DÁN"><img
-								src="<?php echo $themeURL; ?>/assets/images/bang-keo-dan.jpg" alt="BĂNG KEO DÁN"></a></li>
-					<li><a href="https://nhakinhtrongrau.vn/trang-chu" title="Ziczac"><img
-								src="<?php echo $themeURL; ?>/assets/images/ziczac.JPG" alt="Ziczac"></a></li>
-					<li><a href="https://nhakinhtrongrau.vn/trang-chu" title="Đối tác"><img
-								src="<?php echo $themeURL; ?>/assets/images/doi-tac.jpg" alt="Đối tác"></a></li>
-				</ul>
-				<div class="line-box">
-					<div></div>
+				<div class="side-box">
+					<h4 class="title-box"><span>Phụ kiện</span></h4>
+					<ul class="bg-adv main-sub">
+						<li><a href="https://nhakinhtrongrau.vn/trang-chu" title="BĂNG KEO DÁN"><img
+									src="<?php echo $themeURL; ?>/assets/images/bang-keo-dan.jpg" alt="BĂNG KEO DÁN"></a></li>
+						<li><a href="https://nhakinhtrongrau.vn/trang-chu" title="Ziczac"><img
+									src="<?php echo $themeURL; ?>/assets/images/ziczac.JPG" alt="Ziczac"></a></li>
+						<li><a href="https://nhakinhtrongrau.vn/trang-chu" title="Đối tác"><img
+									src="<?php echo $themeURL; ?>/assets/images/doi-tac.jpg" alt="Đối tác"></a></li>
+					</ul>
 				</div>
 			</div>
